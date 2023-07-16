512e2d9495fd02c69d635367edc1b2a4382f5367
Serkan K <serkankas98@gmail.com>
2023-07-16 20:56:18 +0300

    changing configurations

--- kanban_backend/settings.py
+++ kanban_backend/settings.py
@@ -6,7 +6,7 @@ from django.conf import settings
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kytss)93-x0xa(ew-2i0nce_o70t1)keiu0_63!$@nr0yn7&t+'
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     # Third Party Apps
@@ -90,13 +90,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Additional Configurations
 
-ASGI_APPLICATION = 'kanban_backend.wsgi.application'
+ASGI_APPLICATION = 'kanban_backend.asgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
+        
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    )
 }
 
 CHANNEL_LAYERS = {
@@ -109,10 +112,10 @@ CHANNEL_LAYERS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=45),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
-    "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
 
     "ALGORITHM": "HS256",
