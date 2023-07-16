529e2e914859198b4ac4723edc1a5832a4a07988
Serkan K <serkankas98@gmail.com>
2023-07-16 19:18:44 +0300

    Authentication part is over

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -1,4 +1,11 @@
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('/token/get/', views.CustomizedTokenObtainPairView.as_view(), name='get_token'),
+    path('/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
 ]
