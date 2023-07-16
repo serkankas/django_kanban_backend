e26b45d757e7ec872743ed8a71cbbdc8f58443ac
Serkan K <serkankas98@gmail.com>
2023-07-16 19:02:18 +0300

    Configuration

--- kanban_backend/asgi.py
+++ kanban_backend/asgi.py
@@ -1,16 +1,12 @@
-"""
-ASGI config for kanban_backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kanban_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # Just HTTP for now. (We can add other protocols later.)
+})
