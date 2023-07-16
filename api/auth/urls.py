a70b217177ab967f39d698d88492871610b76b0b
Serkan K <serkankas98@gmail.com>
2023-07-16 20:33:52 +0300

    correcting URLs

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -6,6 +6,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/token/get/', views.CustomizedTokenObtainPairView.as_view(), name='get_token'),
-    path('/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('token/get/', views.CustomizedTokenObtainPairView.as_view(), name='get_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
 ]
